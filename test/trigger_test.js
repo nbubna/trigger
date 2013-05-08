b991e7505ded33bf28a464b64eeb5346b22b6807
Nathan Bubna <nathan@esha.com>
2013-05-07 20:47:57 -0700

    boost test coverage (still missing jQuery integration, native click/keyup, and promises)

--- test/trigger_test.js
+++ test/trigger_test.js
@@ -1,5 +1,5 @@
 (function($) {
-  /*
+    /*
     ======== A Handy Little QUnit Reference ========
     http://api.qunitjs.com/
 
@@ -18,44 +18,150 @@
       strictEqual(actual, expected, [message])
       notStrictEqual(actual, expected, [message])
       throws(block, [expected], [message])
-  */
+    */
 
-  module('trigger.js', {
-    // This will run before each test in this module.
-    setup: function() {
-      this.elems = $('#qunit-fixture').children();
+    var trigger = window.trigger,
+      _ = trigger._;
+
+    function on(type, listener) {
+        _.on(type, listener);
     }
-  });
-
-  test('is present', function() {
-    expect(1);
-    ok(window.trigger);
-  });
-
-  /*test('is awesome', function() {
-    expect(1);
-    strictEqual(this.elems.awesome().text(), 'awesome0awesome1awesome2', 'should be awesome');
-  });
-
-  module('jQuery.awesome');
-
-  test('is awesome', function() {
-    expect(2);
-    strictEqual($.awesome(), 'awesome.', 'should be awesome');
-    strictEqual($.awesome({punctuation: '!'}), 'awesome!', 'should be thoroughly awesome');
-  });
-
-  module(':awesome selector', {
-    // This will run before each test in this module.
-    setup: function() {
-      this.elems = $('#qunit-fixture').children();
+    function off(type, listener) {
+        if (document.removeEventListener) {
+            document.removeEventListener(type, listener);
+        } else {
+            document.detachEvent('on'+type, listener);
+        }
     }
-  });
 
-  test('is awesome', function() {
-    expect(1);
-    // Use deepEqual & .get() when comparing jQuery objects.
-    deepEqual(this.elems.filter(':awesome').get(), this.elems.last().get(), 'knows awesome when it sees it');
-  });*/
+    function expectSilence(type) {
+        var fail = function() {
+            ok(false, 'There should not have been a "'+type+'" event.');
+            off(type, fail);
+        };
+        on(type, fail);
+    }
+
+    function listenFor(type, category, data, tags, fn) {
+        var listener = function(e) {
+            off(type, listener);
+            ok(e);
+            strictEqual(e.type, type, 'should be '+type);
+            if (category) {
+                strictEqual(e.category, category, 'should be '+category);
+            }
+            if (data) {
+                deepEqual(e.data, data, 'data mismatch');
+            }
+            if (tags) {
+                tags = tags.split(' ');
+                for (var tag in tags) {
+                    ok(e[tags[tag]], 'event should have '+tags[tag]);
+                }
+                deepEqual(e.tags, tags, 'should have same tags');
+            }
+            if (fn) {
+                if (!fn.apply(this, arguments)) {
+                    _.stop(e, true);
+                }
+            }
+        };
+        on(type, listener);
+    }
+
+    function create(trigger, element) {
+        if (!element){ element = 'button'; }
+        trigger = trigger.replace(/"/g, "'");
+        var html = '<'+element+' '+_.attr+'="'+trigger+'">'+trigger+'</'+element+'>',
+            $el = $(html).appendTo($(document.body));
+        strictEqual($(element+'['+_.attr+'="'+trigger+'"]').length, 1, 'failed to create element');
+        return $el;
+    }
+
+    function triggerElement(trigger, element) {
+        var $el = create(trigger, element);
+        window.trigger($el[0]);
+        $el.remove();
+    }
+
+    function suite(triggerFn, fnExpects, thorough) {
+        test('single event', function() {
+            expect(2+fnExpects);
+            listenFor('single');
+            triggerFn('single');
+        });
+        if (thorough !== false) {
+            test('two events', function() {
+                expect(4+fnExpects);
+                listenFor('one');
+                listenFor('two');
+                triggerFn('one two');
+            });
+            test('categorized event', function() {
+                expect(3+fnExpects);
+                listenFor('categorized', 'category');
+                triggerFn('category:categorized');
+            });
+            test('event with data', function() {
+                expect(3+fnExpects);
+                listenFor('data', null, [1,true,'a','b.c',{d:'#'}, [[{}]]]);
+                triggerFn('data[1,true,"a",'+"'b.c',"+'{"d":"#"},[[{}]]]');
+            });
+            test('tagged event', function() {
+                expect(5+fnExpects);
+                listenFor('tagged', null, null, 'tag1 tag2');
+                triggerFn('tagged#tag1#tag2');
+            });
+            test('everything', function() {
+                expect(6+fnExpects);
+                listenFor('everything', 'category', ['data'], 'tag');
+                triggerFn("category:everything['data']#tag");
+            });
+        }
+        test('cancel event', function() {
+            expect(2+fnExpects);
+            listenFor('pass', null, null, null, function(){ return false; });
+            expectSilence('fail');
+            triggerFn('pass fail');
+        });
+    }
+    
+    module('api');
+    test('user', function() {
+        expect(1, 'user api changed, bump major version');
+        ok(trigger, 'no trigger()');
+    });
+    test('developer', function() {
+        expect(20, 'developer api changed, bump minor version');
+        ok(_, 'no _ object');
+        var props = 'version attr splitRE noClickRE noEnterRE buttonRE boxRE'.split(' ');
+        for (var i=0,m=props.length; i<m; i++) {
+            ok(props[i] in _, 'missing property "'+props[i]+'"');
+        }
+        var fns = 'all parse event stop listen find click keyup on init prop'.split(' ');
+        for (i=0,m=fns.length; i<m; i++) {
+            ok($.isFunction(_[fns[i]]), 'missing function "'+fns[i]+'"');
+        }
+    });
+
+    module('trigger(event)');
+    suite(trigger, 0);
+
+    module('trigger(el, event)');
+    suite(triggerElement, 1);
+
+    module('data-trigger(el, event)');
+    _.attr = 'data-trigger';
+    suite(triggerElement, 1, false);
+    _.attr = 'trigger';
+
+    module('native trigger', {
+        setup: function() {// This will run before each test in this module.
+          //TODO?
+        }
+    });
+    //suite(triggerNative, 1);
+
+    module('jQuery trigger');
 
 }(jQuery));
